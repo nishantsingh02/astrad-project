1272a60f0581d90d9976140ef2f2f972a568f201
Nishant Singh <nra1908@gmail.com>
2025-05-14 14:19:54 +0530

    resolve the routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+
 import Home from '../components/hero';
 import Form from '../components/form';
+import {BrowserRouter, Routes, Route , Link, useNavigate, Outlet} from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/generate" element={<Form />} />
     </Routes>
+    </BrowserRouter>
   );
 }
 
