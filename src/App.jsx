ae4867d9b4126d06acf496526187debd45d00a22
Nishant Singh <nra1908@gmail.com>
2025-05-14 12:57:47 +0530

    done with landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
 
 function App() {
-
   return (
     <div>
-      hi there!
+      <div className="text-xl">Astrad</div>
+      <div className="p-10">
+        <h1>What if your <span className="text-yellow-500">birth chart</span></h1>
+        <h1>made you a <span className="text-yellow-500">here</span>?</h1>
+      </div>
+      <div>
+        <button className="border-2 font border-yellow-400 p-4 rounded-full bg-yellow-600 text-white shadow-md hover:shadow-yellow-500 hover:scale-105 transition duration-300">
+  Generate My Character
+</button>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
