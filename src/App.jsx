11a3fedbe96f4fe4dd61e3459aba531dcc44dffa
Rishi6jp <rainbro187@gmail.com>
2025-05-14 15:48:02 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Home from '../components/Hero';
 import Form from '../components/Form';
-import Card from '../components/Card'
+import Card from '../components/Card';
 import {BrowserRouter, Routes, Route , Link, useNavigate, Outlet} from "react-router-dom";
 
 function App() {
